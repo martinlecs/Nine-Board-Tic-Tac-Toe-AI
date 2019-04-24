bfdc8f41560dc3cebb14b4483febea9cf3e62d38
Arsha Abayasingam <arsha.abayasingam@unsw.edu.au>
2019-04-24 12:28:52 +1000

    commented out the debug statements, please dont remove yet

--- player/heuristic/Ash_Heuristic.py
+++ player/heuristic/Ash_Heuristic.py
@@ -74,89 +74,89 @@ class Heuristic:
         diagonal_one = diagonal_two = opponent_diagonal_one = opponent_diagonal_two = winner = loser = 0
 
         if board[1] == 1 and board[5] == 0 and board[9] == 0:
-            print ("In diagonal if1")
+            ##print ("In diagonal if1")
             diagonal_one += 1
         elif board[1] == 0 and board[5] == 1 and board[9] == 0:
-            print ("In diagonal if2")
+            ##print ("In diagonal if2")
             diagonal_one += 1
         elif board[1] == 0 and board[5] == 0 and board[9] == 1:
-            print ("In diagonal if3")
+            ##print ("In diagonal if3")
             diagonal_one += 1
         elif board[1] == 1 and board[5] == 1 and board[9] == 0:
-            print ("In diagonal if4")
+            ##print ("In diagonal if4")
             diagonal_two += 1
         elif board[1] == 0 and board[5] == 1 and board[9] == 1:
-            print ("In diagonal if5")
+            ##print ("In diagonal if5")
             diagonal_two += 1
         elif board[1] == 1 and board[5] == 0 and board[9] == 1:
-            print ("In diagonal if6")
+            ##print ("In diagonal if6")
             diagonal_two += 1
         elif board[1] == 1 and board[5] == 1 and board[9] == 1:
-            print ("In diagonal if7")
+            ##print ("In diagonal if7")
             winner += 1
         if board[3] == 1 and board[5] == 0 and board[7] == 0:
-            print ("In diagonal if8")
+            ##print ("In diagonal if8")
             diagonal_one += 1
         elif board[3] == 0 and board[5] == 1 and board[7] == 0:
-            print ("In diagonal if9")
+            ##print ("In diagonal if9")
             diagonal_one += 1
         elif board[3] == 0 and board[5] == 0 and board[7] == 1:
-            print ("In diagonal if10")
+            ##print ("In diagonal if10")
             diagonal_one += 1
         elif board[3] == 1 and board[5] == 1 and board[7] == 0:
-            print ("In diagonal if11")
+            ##print ("In diagonal if11")
             diagonal_two += 1
         elif board[3] == 0 and board[5] == 1 and board[7] == 1:
-            print ("In diagonal if12")
+            ##print ("In diagonal if12")
             diagonal_two += 1
         elif board[3] == 1 and board[5] == 0 and board[7] == 1:
-            print ("In diagonal if13")
+            ##print ("In diagonal if13")
             diagonal_two += 1
         elif board[3] == 1 and board[5] == 1 and board[7] == 1:
-            print ("In diagonal if14")
+            ##print ("In diagonal if14")
             winner += 1
 
         if board[1] == -1 and board[5] == 0 and board[9] == 0:
-            print ("In diagonal if15")
+            ##print ("In diagonal if15")
             opponent_diagonal_one += 1
         elif board[1] == 0 and board[5] == -1 and board[9] == 0:
-            print ("In diagonal if16")
+            ##print ("In diagonal if16")
             opponent_diagonal_one += 1
         elif board[1] == 0 and board[5] == 0 and board[9] == -1:
-            print ("In diagonal if17")
+            ##print ("In diagonal if17")
             opponent_diagonal_one += 1
         elif board[1] == -1 and board[5] == -1 and board[9] == 0:
-            print ("In diagonal if18")
+            ##print ("In diagonal if18")
             opponent_diagonal_two += 1
         elif board[1] == 0 and board[5] == -1 and board[9] == -1:
-            print ("In diagonal if19")
+            ##print ("In diagonal if19")
             opponent_diagonal_two += 1
         elif board[1] == -1 and board[5] == 0 and board[9] == -1:
-            print ("In diagonal if20")
+            ##print ("In diagonal if20")
             opponent_diagonal_two += 1
         elif board[1] == -1 and board[5] == -1 and board[9] == -1:
-            print ("In diagonal if21")
+            ##print ("In diagonal if21")
             loser += 1
         if board[3] == -1 and board[5] == 0 and board[7] == 0:
-            print ("In diagonal if22")
+            ##print ("In diagonal if22")
             opponent_diagonal_one += 1
         elif board[3] == 0 and board[5] == -1 and board[7] == 0:
-            print ("In diagonal if23")
+            ##print ("In diagonal if23")
             opponent_diagonal_one += 1
         elif board[3] == 0 and board[5] == 0 and board[7] == -1:
-            print ("In diagonal if24")
+            ##print ("In diagonal if24")
             opponent_diagonal_one += 1
         elif board[3] == -1 and board[5] == -1 and board[7] == 0:
-            print ("In diagonal if25")
+            ##print ("In diagonal if25")
             opponent_diagonal_two += 1
         elif board[3] == 0 and board[5] == -1 and board[7] == -1:
-            print ("In diagonal if26")
+            ##print ("In diagonal if26")
             opponent_diagonal_two += 1
         elif board[3] == -1 and board[5] == 0 and board[7] == -1:
-            print ("In diagonal if27")
+            ##print ("In diagonal if27")
             opponent_diagonal_two += 1
         elif board[3] == -1 and board[5] == -1 and board[7] == -1:
-            print ("In diagonal if28")
+            ##print ("In diagonal if28")
             loser += 1
 
         # TODO: consider replacing this with a dict
@@ -181,47 +181,47 @@ class Heuristic:
 
         for x in range(1, 4):
             if board[x] == 1 and board[x + 3] == 0 and board[x + 6] == 0:
-                print ("in vertical if 1 with column %d" %(x))
+                #print ("in vertical if 1 with column %d" %(x))
                 vertical_one += 1
             elif board[x] == 0 and board[x + 3] == 1 and board[x + 6] == 0:
-                print("in vertical if 2 with column %d" % (x))
+                #print("in vertical if 2 with column %d" % (x))
                 vertical_one += 1
             elif board[x] == 0 and board[x + 3] == 0 and board[x + 6] == 1:
-                print("in vertical if 3 with column %d" % (x))
+                #print("in vertical if 3 with column %d" % (x))
                 vertical_one += 1
             elif board[x] == 1 and board[x + 3] == 1 and board[x + 6] == 0:
-                print("in vertical if 4 with column %d" % (x))
+                #print("in vertical if 4 with column %d" % (x))
                 vertical_two += 1
             elif board[x] == 0 and board[x + 3] == 1 and board[x + 6] == 1:
-                print("in vertical if 5 with column %d" % (x))
+                #print("in vertical if 5 with column %d" % (x))
                 vertical_two += 1
             elif board[x] == 1 and board[x + 3] == 0 and board[x + 6] == 1:
-                print("in vertical if 6 with column %d" % (x))
+                #print("in vertical if 6 with column %d" % (x))
                 vertical_two += 1
             elif board[x] == 1 and board[x + 3] == 1 and board[x + 6] == 1:
-                print("in vertical if 7 with column %d" % (x))
+                #print("in vertical if 7 with column %d" % (x))
                 winner += 1
 
             if board[x] == -1 and board[x + 3] == 0 and board[x + 6] == 0:
-                print("in vertical if 8 with column %d" % (x))
+                #print("in vertical if 8 with column %d" % (x))
                 opponent_vertical_one += 1
             elif board[x] == 0 and board[x + 3] == -1 and board[x + 6] == 0:
-                print("in vertical if 9 with column %d" % (x))
+                #print("in vertical if 9 with column %d" % (x))
                 opponent_vertical_one += 1
             elif board[x] == 0 and board[x + 3] == 0 and board[x + 6] == -1:
-                print("in vertical if 10 with column %d" % (x))
+                #print("in vertical if 10 with column %d" % (x))
                 opponent_vertical_one += 1
             elif board[x] == -1 and board[x + 3] == -1 and board[x + 6] == 0:
-                print("in vertical if 11 with column %d" % (x))
+                #print("in vertical if 11 with column %d" % (x))
                 opponent_vertical_two += 1
             elif board[x] == 0 and board[x + 3] == -1 and board[x + 6] == -1:
-                print("in vertical if 12 with column %d" % (x))
+                #print("in vertical if 12 with column %d" % (x))
                 opponent_vertical_two += 1
             elif board[x] == -1 and board[x + 3] == 0 and board[x + 6] == -1:
-                print("in vertical if 13 with column %d" % (x))
+                #print("in vertical if 13 with column %d" % (x))
                 opponent_vertical_two += 1
             elif board[x] == -1 and board[x + 3] == -1 and board[x + 6] == -1:
-                print("in vertical if 14 with column %d" % (x))
+                #print("in vertical if 14 with column %d" % (x))
                 loser += 1
 
         return vertical_one, vertical_two, opponent_vertical_one, opponent_vertical_two, winner, loser
@@ -246,47 +246,47 @@ class Heuristic:
         digits = [1, 4, 7]
         for x in digits:
             if board[x] == 1 and board[x + 1] == 0 and board[x + 2] == 0:
-                print("in horizontal if 1 with row %d" % (x))
+                #print("in horizontal if 1 with row %d" % (x))
                 horizontal_one += 1
             elif board[x] == 0 and board[x + 1] == 1 and board[x + 2] == 0:
-                print("in horizontal if 2 with row %d" % (x))
+                #print("in horizontal if 2 with row %d" % (x))
                 horizontal_one += 1
             elif board[x] == 0 and board[x + 1] == 0 and board[x + 2] == 1:
-                print("in horizontal if 3 with row %d" % (x))
+                #print("in horizontal if 3 with row %d" % (x))
                 horizontal_one += 1
             elif board[x] == 1 and board[x + 1] == 1 and board[x + 2] == 0:
-                print("in horizontal if 4 with row %d" % (x))
+                #print("in horizontal if 4 with row %d" % (x))
                 horizontal_two += 1
             elif board[x] == 0 and board[x + 1] == 1 and board[x + 2] == 1:
-                print("in horizontal if 5 with row %d" % (x))
+                #print("in horizontal if 5 with row %d" % (x))
                 horizontal_two += 1
             elif board[x] == 1 and board[x + 1] == 0 and board[x + 2] == 1:
-                print("in horizontal if 6 with row %d" % (x))
+                #print("in horizontal if 6 with row %d" % (x))
                 horizontal_two += 1
             elif board[x] == 1 and board[x + 1] == 1 and board[x + 2] == 1:
-                print("in horizontal if 7 with row %d" % (x))
+                #print("in horizontal if 7 with row %d" % (x))
                 winner += 1
 
             if board[x] == -1 and board[x + 1] == 0 and board[x + 2] == 0:
-                print("in horizontal if 8 with row %d" % (x))
+                #print("in horizontal if 8 with row %d" % (x))
                 opponent_horizontal_one += 1
             elif board[x] == 0 and board[x + 1] == -1 and board[x + 2] == 0:
-                print("in horizontal if 9 with row %d" % (x))
+                #print("in horizontal if 9 with row %d" % (x))
                 opponent_horizontal_one += 1
             elif board[x] == 0 and board[x + 1] == 0 and board[x + 2] == -1:
-                print("in horizontal if 10 with row %d" % (x))
+                #print("in horizontal if 10 with row %d" % (x))
                 opponent_horizontal_one += 1
             elif board[x] == -1 and board[x + 1] == -1 and board[x + 2] == 0:
-                print("in horizontal if 11 with row %d" % (x))
+                #print("in horizontal if 11 with row %d" % (x))
                 opponent_horizontal_two += 1
             elif board[x] == 0 and board[x + 1] == -1 and board[x + 2] == -1:
-                print("in horizontal if 12 with row %d" % (x))
+                #print("in horizontal if 12 with row %d" % (x))
                 opponent_horizontal_two += 1
             elif board[x] == -1 and board[x + 1] == 0 and board[x + 2] == -1:
-                print("in horizontal if 13 with row %d" % (x))
+                #print("in horizontal if 13 with row %d" % (x))
                 opponent_horizontal_two += 1
             elif board[x] == -1 and board[x + 1] == -1 and board[x + 2] == -1:
-                print("in horizontal if 14 with row %d" % (x))
+                #print("in horizontal if 14 with row %d" % (x))
                 loser += 1
 
         return horizontal_one, horizontal_two, opponent_horizontal_one, opponent_horizontal_two, winner, loser
@@ -314,11 +314,11 @@ class Heuristic:
 
         heuristic = self._win * winner + self._lose * loser + self._alpha * my_two + self._beta * my_one - self._gamma * opp_two - self._delta * opp_one
 
-        print(my_two)
-        print (my_one)
-        print(opp_two)
-        print (opp_one)
-        print ("****")
+        #print(my_two)
+        #print (my_one)
+        #print(opp_two)
+        #print (opp_one)
+        #print ("****")
         return heuristic
 
     def heuristic(self):
@@ -330,8 +330,8 @@ class Heuristic:
         """
         total_heuristic = 0
         for board in self._global_board:
-            print (total_heuristic)
-            print("!!!!!")
+            #print (total_heuristic)
+            #print("!!!!!")
             total_heuristic += self.__calculate_board_heuristic(board)
 
         return total_heuristic
