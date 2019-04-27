81343455211fd14f3be0c563c051306de2a8c62e
Arsha Abayasingam <arsha.abayasingam@unsw.edu.au>
2019-04-27 22:34:26 +1000

    corrected hash define function

--- player/Hash_function.py
+++ player/Hash_function.py
@@ -4,10 +4,13 @@ import numpy as np
 def __hash_function(board: np.ndarray):
     hash = 0
     for x in range(1, 10):
-        hash +=  (board[x]*3**x)
+        #print (board[x] + 1)
+        #print ((board[x] + 1)*3**x)
+        hash +=  ((board[x] + 1)*3**x)
 
     return hash
 
 if __name__ == "__main__":
     h = __hash_function([0, 1, 0, 0, 1, -1, 0, 0, 0, 0])
+    #print(h)
 
