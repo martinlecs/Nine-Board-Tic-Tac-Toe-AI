d48bc1c10e4e65ecfffdc1d708d9a11b3645af43
Arsha Abayasingam <arsha.abayasingam@unsw.edu.au>
2019-04-27 15:21:08 +1000

    hash_function

--- player/Hash_function.py
+++ player/Hash_function.py
@@ -2,18 +2,6 @@ import numpy as np
 
 
 def __hash_function(board: np.ndarray):
-    """ Calculates the heuristic value for each vertical in a Tic-Tac-Toe board.
-
-
-    Args:
-        board (numpy.ndarray): The current Tic-Tac-Toe board that we are currently playing on,
-                              np.ndarray has shape (10,).
-
-    Returns:
-        vertical_one, vertical_two, opponent_vertical_one, opponent_vertical_two, winner, loser
-        (int, int, int, int, int, int): Tuple contains the heuristic values calculated for a Tic-Tac-Toe board's verticals.
-
-    """
     hash = 0
     for x in range(1, 10):
         hash +=  (board[x]*3**x)
