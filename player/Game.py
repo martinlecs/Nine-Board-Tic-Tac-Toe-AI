fixed issue with incorrect terminal node calculation

player/Game.py
@@ -43,12 +43,23 @@ class Game:
         """ Returns the corresponding hash value for a board
             board (numpy array): i1
         """
-
         return self._board_hashes[board.tostring()]
 
-    def is_terminal(self, state: np.ndarray):
+    def hash_to_board(self, hash: int):
+        """ Converts a hash value back into its original board
+        :param hash:
+        :return: Read-only np array
+        """
+        return np.frombuffer(self._hash_to_board[hash], dtype='i1')
+
+    def is_terminal(self, node: GameTreeNode):
         """ Checks if there is a terminal node in a given global game state """
-        return any([self._win_states[s] for s in state])
+        if not node.parent:
+            board = node.board
+        else:
+            board = node.state[node.parent]
+
+        return self._win_states[board]  # TODO this is wrong?
 
     @staticmethod
     def is_terminal_node(board: np.ndarray):
@@ -109,41 +120,31 @@ class Game:
 
         self._win_states = win_states_dict
 
-    def generate_moves(self, state: np.ndarray, curr_board: int, player: int, eval_fn: Heuristic, depth: int ):
+    def generate_moves(self, state: np.ndarray, curr_board: int, player: int):
         """ Generates all possible moves for current player by looking at empty squares as potential moves
             Player 1 = 1, Player 2 = -1
 
         """
-
         # create local copy current board in play
-        board = np.frombuffer(self._hash_to_board[state[curr_board]], dtype='i1')   # read-only
+        board = np.frombuffer(self._hash_to_board[state[curr_board]], dtype='i1')  # read-only
         modifiable_board = np.empty_like(board)
         modifiable_board[:] = board
 
-        move_list = []
-        for i in range(1, 10):
-            if modifiable_board[i] == 0:
+        # create a local copy of the global state
+        updated_state = np.empty_like(state)
+        updated_state[:] = state
 
-                # set board
-                modifiable_board[i] = player
+        for i in np.where(modifiable_board == 0)[0][1:]:
+            # set board
+            modifiable_board[i] = player
 
-                # create a copy of global state and pass that down to the child
-                updated_state = np.empty_like(state)
-                updated_state[:] = state
-                updated_state[curr_board] = self.board_to_hash(modifiable_board)
+            # create a copy of global state and pass that down to the child
+            prev_board = updated_state[curr_board]
+            updated_state[curr_board] = self.board_to_hash(modifiable_board)
 
-                # calculate child's heuristic value and append child to parent
-                g = GameTreeNode(updated_state, i, player)
-                depth = 1 if depth == 0 else depth
-                g.heuristic_val = eval_fn.compute_heuristic(updated_state, depth)
-                move_list.append(g)
+            yield GameTreeNode(updated_state, i, parent=curr_board)
 
-                # reset board
-                modifiable_board[i] = 0
-
-        # order children
-        reversed = True if player == 1 else False
-        move_list.sort(key=lambda x: x.heuristic_val, reverse=reversed)
-
-        return move_list
+            # reset board
+            modifiable_board[i] = 0
+            updated_state[curr_board] = prev_board
 

player/Heuristic.py
@@ -196,7 +196,7 @@ class Heuristic:
             heuristic (int): Heuristic value of the board
 
         """
-        diagonal_one, diagonal_two, opponent_diagonal_one, opponent_diagonal_two, winner_d, loser_d = self.__calculate_diagonal(
+        diagonal_one, diagonal_two, opponent_diagonal_one, opponent_diagonal_two, winner_d, loser_d = self.calculate_diagonal(
             board)
         vertical_one, vertical_two, opponent_vertical_one, opponent_vertical_two, winner_v, loser_v = self.__calculate_vertical(
             board)
@@ -225,19 +225,15 @@ class Heuristic:
 
     def __precompute_heuristic_values(self):
 
-        # generate all possible states
-        num_to_select = 9  # number of squares in tic-tac-toe board
-        possible_values = [0, 1, -1]
-        result = itertools.product(possible_values, repeat=num_to_select)  # creates a generator
+        # generate all possible boards
+        result = itertools.product([0, 1, -1], repeat=9)  # creates a generator
 
         heuristic_dict = {}
-        hash_board = {}
         for counter, i in enumerate(result):
             i = list(i)
             i.insert(0, 0)  # add leading zero to match formatting of np.array in agent.py
             if i.count(1) < 5 or i.count(-1) < 5:
                 heuristic_dict[counter] = self.__calculate_board_heuristic(np.array(i, dtype='i1'))
-                hash_board[counter] = np.array(i, dtype='i1')
 
         with open(os.path.join(SAVE_PATH, 'heuristic_values.pickle'), 'wb') as file:
             pickle.dump(heuristic_dict, file)

tests/test_alphabeta.py
@@ -195,7 +195,7 @@ def test_negamax_avoid_loss_in_next_turn_2(game_cls, heuristic_func):
     parameterized_state = np.array([game_cls.board_to_hash(b) for b in state])
 
     start_node = GameTreeNode(parameterized_state, 1)
-    m = AlphaBeta(start_node, game_cls, heuristic_func, 7)
+    m = AlphaBeta(start_node, game_cls, heuristic_func, 5)
     best_move = m.run()
 
     # print_depth_1_nodes(start_node, best_move, m.nodes_generated)
@@ -306,4 +306,5 @@ if __name__ == "__main__":
     heuristic.load()
     game = Game()
     game.load()
-    cProfile.runctx('g(x, y)', {'y': heuristic, 'x': game, 'g': test_negamax_avoid_loss_in_next_turn_2}, {})
+    # cProfile.runctx('g(x, y)', {'y': heuristic, 'x': game, 'g': test_negamax_avoid_loss_in_next_turn_2}, {})
+    cProfile.runctx('g(x, y)', {'y': heuristic, 'x': game, 'g': test_avoid_loss_in_next_move_5}, {})